7b771cbe2624885eec2e74735b1ed3625bfd67d1
Charles <csharpe124@gmail.com>
2020-04-01 01:39:42 +0100

    Changed the proces for the encoder, removed much of the complexity e.g. conversion, I was being stupid

--- Encoder Calc/Encoder calculator.py
+++ Encoder Calc/Encoder calculator.py
@@ -1,39 +1,19 @@
 Pulse = int(input("How many pulses have you measured?;"))
-Distance = str(input("Measured over what distance?;"))
-DisType = int(input("If measurement distance was in Meter press 1\nIf measurement was in CM pres 2\n"
-                    "if measurement was in inch press3\n"))
-
-AltDistance = 1
-
-if DisType == 1:
-    print("You measured in meters")
-        # Convert to 1M then convert to inchs at 39.37 inch to 1 M
-    if Distance == 1:
-        AltDistance = 39.37
-        print(AltDistance)
-    elif Distance < str(1):
- #       AltDistance = ((Distance * (1/39.37))
-        print("less than 1", AltDistance)
-    else :
-        print("END")
-
-elif DisType == 2:
-    print("You measured in CM")
-        #Convert to Meters then inchs
-
-elif DisType == 3:
-
-    print("Your measurment was in inchs")
-        # Convert to 39.37
-
-
-else:
-    print("Not Valid")
-
-
-PPI = Pulse / AltDistance
-print("Pulse per inch = ", PPI)
+Distance = str(input("Distance in CM?;"))
 DPI = int(input("What DPI do you want to print at?"))
+
+CM_PER_INCH = 2.54
+Distance_Inch = (int(Distance) / int(CM_PER_INCH))
+
+print("Distance in Inchs", Distance_Inch)
+
+# I was being to complicated, all I need to do is convert input measurment 
+# into inchs, then divide number of pulse by the new value...
+
+PPI = (Pulse / Distance_Inch)
+
+print("Pulse per inch = ", PPI)
+
 PulsePerStroke = PPI / DPI
 print("Pulse per stroke; ", PulsePerStroke)
 Multiplier = 1
@@ -44,7 +24,45 @@ if (Divider.is_integer()):
     print("Divider, =", Divider)
 
 
-#
-#test1 = 46.9898038103
-#print((test1).is_integer())
-#
+
+
+
+
+# ####################################################################
+# #AltDistance = 1
+# # if DisType == 1:
+# #     print("You measured in meters")
+# #         # Convert to 1M then convert to inchs at 39.37 inch to 1 M
+# #     if Distance == 1:
+# #         AltDistance = 39.37
+# #         print(AltDistance)
+# #     elif Distance < str(1):
+# #  #       AltDistance = ((Distance * (1/39.37))
+# #         print("less than 1", AltDistance)
+# #     else :
+# #         print("END")
+# # elif DisType == 2:
+# #     print("You measured in CM")
+# #         #Convert to Meters then inchs
+# # elif DisType == 3:
+# #     print("Your measurment was in inchs")
+# #         # Convert to 39.37
+# # else:
+# #     print("Not Valid")
+# #####################################################################
+
+# AltDistance = 1
+
+# PPI = Pulse / AltDistance
+# print("Pulse per inch = ", PPI)
+# DPI = int(input("What DPI do you want to print at?"))
+# PulsePerStroke = PPI / DPI
+# print("Pulse per stroke; ", PulsePerStroke)
+# Multiplier = 1
+# Divider = Multiplier * PulsePerStroke
+
+# if (Divider.is_integer()):
+#     print("Multiplier = ",Multiplier)
+#     print("Divider, =", Divider)
+
+

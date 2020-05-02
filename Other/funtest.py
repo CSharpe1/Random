281db6345ebdba30f4bc84ee1df56ed98c51e824
Charles <csharpe124@gmail.com>
2020-05-02 19:02:36 +0100

    doc testing

---
+++ Other/funtest.py
@@ -0,0 +1,36 @@
+def func(x):
+    """
+    This function adds +1 to an input
+
+    Arguments:
+        x {int} -- [a description about the func function]
+
+    Returns:
+        [int] -- [x+1]
+    """
+    return x + 1
+
+def double(x):
+    """[Doubles the input]
+
+    Arguments:
+        x {[int]} -- [int input for double()]
+
+    Returns:
+        [value of x*2] -- [for example x = 2 return = 4]
+    """
+    return x * 2
+
+
+def less(x):
+    '''[x - 1]
+
+    Arguments:
+        x {[int]} -- [input value]
+
+    Returns:
+        [int] -- [for example x = 2 retunrs 1]
+    '''
+    return x - 1
+
+print(less(4))

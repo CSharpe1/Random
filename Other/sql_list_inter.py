194dee9f7e8e8077193bc03d3ceac9b3663d75a5
Charles <csharpe124@gmail.com>
2020-05-03 15:21:56 +0100

    example while loop iter

---
+++ Other/sql_list_inter.py
@@ -0,0 +1,7 @@
+list_test = ['PSA Peugeot Citroen','Suzuki','Fiat Chrysler Automobiles','Honda','Ford','Hyundai-Kia', 'General Motors' 'Renault-Nissan','Toyota','VW']
+
+count = 0
+while count < (len(list_test)):
+    print(" This is my SQL test,",list_test[count])
+    print(count)
+    count += 1
